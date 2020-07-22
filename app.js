e575752b3a868697add467d59be94c5cac245d3c
Krystel Baca <kbaca@foundationmedicine.com>
2020-07-21 23:02:13 -0500

    refactor app server, added nnodemon and setup jasmine

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const express = require('express')
+
+const port = 3002
+const host = 'localhost'
+
+const app = express()
+
+const validRoutes = () => {
+  app.use('/api/v1/health', require('./api/health'))
+}
+
+validRoutes()
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Music Web Api - Hello World!')
+})
+
+const server = http.createServer(app)
+
+server.listen(port, host)
+console.log(`Running node app on http://${host}:${port}`)
+
+module.exports = app
